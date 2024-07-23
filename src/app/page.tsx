95261a09c83e7d5cf9fdcea061afeca46dfbfc2c
jay-prajapati <jay.n.prajapati@bacancy.com>
2024-07-23 15:23:01 +0530

    [FEAT]: common form component with zod and react-hook-form validation added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,152 +1,47 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog';
-import { ToastAction } from '@/components/ui/toast';
-import { useToast } from '@/components/ui/use-toast';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import Heading1 from '@/components/ui/headings/Heading1';
 import Heading2 from '@/components/ui/headings/Heading2';
 import Heading3 from '@/components/ui/headings/Heading3';
-import ThemeSwitch from '@/components/custom/ThemeSwitch';
-export default function Home() {
-  const { toast } = useToast();
+import { ECommerceForm } from '@/components/ui/common/ECommerceForm';
+import { z } from 'zod';
 
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+  suburb: z.string({
+    required_error: 'Please select an email to display.',
+  }),
+});
+
+export default function Home() {
   return (
     <>
-      <ThemeSwitch />
-      <Heading1 className=''>
-        Hello I am Jay
-      </Heading1>
-      <Heading2 className=''>
-        Hello I am Jay
-      </Heading2>
-      <Heading3 className=''>
-        Hello I am Jay
-      </Heading3>
-      <div className="container my-10">
-        <Button className="">Click Me</Button>
-        <Tabs defaultValue="account" className="mx-auto w-[400px]">
-          <TabsList className="grid w-full grid-cols-2">
-            <TabsTrigger value="account">Account</TabsTrigger>
-            <TabsTrigger value="password">Password</TabsTrigger>
-          </TabsList>
-          <TabsContent value="account">
-            <Card>
-              <CardHeader>
-                <CardTitle>Account</CardTitle>
-                <CardDescription>
-                  Make changes to your account here. Click save when you're
-                  done.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="space-y-2">
-                <div className="space-y-1">
-                  <Label htmlFor="name">Name</Label>
-                  <Input id="name" defaultValue="Pedro Duarte" />
-                </div>
-                <div className="space-y-1">
-                  <Label htmlFor="username">Username</Label>
-                  <Input id="username" defaultValue="@peduarte" />
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button>Save changes</Button>
-              </CardFooter>
-            </Card>
-          </TabsContent>
-          <TabsContent value="password">
-            <Card>
-              <CardHeader>
-                <CardTitle>Password</CardTitle>
-                <CardDescription>
-                  Change your password here. After saving, you'll be logged out.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="space-y-2">
-                <div className="space-y-1">
-                  <Label htmlFor="current">Current password</Label>
-                  <Input id="current" type="password" />
-                </div>
-                <div className="space-y-1">
-                  <Label htmlFor="new">New password</Label>
-                  <Input id="new" type="password" />
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button>Save password</Button>
-              </CardFooter>
-            </Card>
-          </TabsContent>
-        </Tabs>
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button variant="outline">Edit Profile</Button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Edit profile</DialogTitle>
-              <DialogDescription>
-                Make changes to your profile here. Click save when you're done.
-              </DialogDescription>
-            </DialogHeader>
-            <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="name" className="text-right">
-                  Name
-                </Label>
-                <Input
-                  id="name"
-                  defaultValue="Pedro Duarte"
-                  className="col-span-3"
-                />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="username" className="text-right">
-                  Username
-                </Label>
-                <Input
-                  id="username"
-                  defaultValue="@peduarte"
-                  className="col-span-3"
-                />
-              </div>
-            </div>
-            <DialogFooter>
-              <Button type="submit">Save changes</Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-        <Button
-          variant="outline"
-          onClick={() => {
-            toast({
-              title: 'Uh oh! Something went wrong.',
-              description: 'There was a problem with your request.',
-              action: <ToastAction altText="Try again">Try again</ToastAction>,
-            });
-          }}
-        >
-          Show Toast
-        </Button>
+      <Heading1 className="">Hello I am Jay</Heading1>
+      <Heading2 className="">Hello I am Jay</Heading2>
+      <Heading3 className="">Hello I am Jay</Heading3>
+      <div>
+        <ECommerceForm
+          onSubmit={(e) => console.log(e)}
+          formSchema={formSchema}
+          className="mx-auto max-w-96"
+          elements={[
+            {
+              type: 'input',
+              label: 'Name',
+              key: 'username',
+              placeholder: 'Enter your name',
+            },
+            {
+              type: 'select',
+              label: 'Suburb',
+              key: 'suburb',
+              placeholder: 'select your name',
+              selectItems: ['abc', 'xyz', 'pqr'],
+            },
+          ]}
+        />
       </div>
     </>
   );
